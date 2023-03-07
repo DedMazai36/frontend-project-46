81cf3842515b8672497a144471b3ee14a16e3ac6
Andrey Koliukh <kolldronzr@yandex.ru>
2023-03-07 13:07:37 +0600

    fix smells

--- src/genDiffEngine.js
+++ src/genDiffEngine.js
@@ -4,12 +4,10 @@ import getObjectFromFilePath from './parsers.js';
 const getResultArray = (file1, file2) => {
   const namesOfFile1 = Object.getOwnPropertyNames(file1).sort();
   const namesOfFile2 = Object.getOwnPropertyNames(file2).sort();
-
   const filtredNamesOfFile1 = [...namesOfFile1].filter((name) => {
     if (namesOfFile2.indexOf(name) < 0 || file1[name] === file2[name]) {
       return true;
-    }
-    return false;
+    } return false;
   });
   const filtredNamesOfFile2 = _.difference(namesOfFile2, namesOfFile1);
   const intersectionNames = _.intersection(namesOfFile1, namesOfFile2);
@@ -17,27 +15,21 @@ const getResultArray = (file1, file2) => {
   const resultArray = [];
 
   filtredNamesOfFile1.forEach((name) => {
-    const value = file1[name];
-    const message = `${name}: ${value}`;
-
+    const message = `${name}: ${file1[name]}`;
     if (_.intersection(namesOfFile1, namesOfFile2).indexOf(name) >= 0) {
       resultArray.push(`    ${message}`);
     } else {
       resultArray.push(`  - ${message}`);
     }
   });
-
   resultiIntersection.forEach((name) => {
     const message = `  - ${name}: ${file1[name]}\n  + ${name}: ${file2[name]}`;
     resultArray.push(message);
   });
-
   filtredNamesOfFile2.forEach((name) => {
-    const value = file2[name];
-    const message = `${name}: ${value}`;
+    const message = `${name}: ${file2[name]}`;
     resultArray.push(`  + ${message}`);
   });
-
   return resultArray;
 };
 
