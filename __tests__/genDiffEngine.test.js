3d588d172cec02b16ffab3a86f4ef79bff2f06f7
Andrey Koliukh <kolldronzr@yandex.ru>
2023-03-07 11:15:19 +0600

    add test for yaml

--- __tests__/genDiffEngine.test.js
+++ __tests__/genDiffEngine.test.js
@@ -7,10 +7,18 @@ import genDiff from '../src/genDiffEngine.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const filePath1 = path.join(__dirname, '..', '__fixtures__', 'file1.json');
-const filePath2 = path.join(__dirname, '..', '__fixtures__', 'file2.json');
 const expectValue = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
 
-test('Testing function genDiff', () => {
+test('Testing function genDiff width .json', () => {
+  const filePath1 = path.join(__dirname, '..', '__fixtures__', 'file1.json');
+  const filePath2 = path.join(__dirname, '..', '__fixtures__', 'file2.json');
+
+  expect(genDiff(filePath1, filePath2)).toBe(expectValue);
+});
+
+test('Testing function genDiff width .yaml', () => {
+  const filePath1 = path.join(__dirname, '..', '__fixtures__', 'file1.yaml');
+  const filePath2 = path.join(__dirname, '..', '__fixtures__', 'file2.yaml');
+
   expect(genDiff(filePath1, filePath2)).toBe(expectValue);
 });
