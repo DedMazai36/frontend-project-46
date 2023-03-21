15c203a79f2f42a41e7630996d52d6f98c4d2f3c
Andrey Koliukh <kolldronzr@yandex.ru>
2023-03-21 12:59:30 +0600

    fix plain.js

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { getFiltredNames, isObject } from './formattersEngine.js';
 
 const ifStringAddApostrophe = (str) => {
@@ -9,79 +10,71 @@ const ifStringAddApostrophe = (str) => {
 
 const plain = (object1, object2, path = '') => {
   const arrayOfNames = getFiltredNames(object1, object2);
-  const filtredNamesOfObject1 = arrayOfNames[0];
-  const filtredNamesOfIntersection = arrayOfNames[1];
-  const filtredNamesOfObject2 = arrayOfNames[2];
-  const arrayForSort = arrayOfNames.flat(10).sort();
-  const resultArrayOfStrings = [];
-  arrayForSort.forEach((name) => {
+  const [filtredNamesOfObject1,
+    filtredNamesOfIntersection,
+    filtredNamesOfObject2] = arrayOfNames;
+  const arrayForSort = _.sortBy(arrayOfNames.flat(10));
+
+  const resultArrayOfStrings = arrayForSort.map((name) => {
     const cumulativePath = `${path}.${name}`;
     if (filtredNamesOfObject1.includes(name)) {
       if (!Object.getOwnPropertyNames(object2).includes(name)) {
-        resultArrayOfStrings.push(
+        return (
           `Property '${cumulativePath.slice(
             1,
             cumulativePath.length,
-          )}' was removed`,
-        );
+          )}' was removed`);
       }
     }
     if (filtredNamesOfObject2.includes(name)) {
       if (!isObject(object2[name])) {
-        resultArrayOfStrings.push(
+        return (
           `Property '${cumulativePath.slice(
             1,
             cumulativePath.length,
-          )}' was added with value: ${ifStringAddApostrophe(object2[name])}`,
-        );
-      } else {
-        resultArrayOfStrings.push(
-          `Property '${cumulativePath.slice(
-            1,
-            cumulativePath.length,
-          )}' was added with value: [complex value]`,
-        );
+          )}' was added with value: ${ifStringAddApostrophe(object2[name])}`);
       }
+      return (
+        `Property '${cumulativePath.slice(
+          1,
+          cumulativePath.length,
+        )}' was added with value: [complex value]`);
     }
     if (filtredNamesOfIntersection.includes(name)) {
       if (!isObject(object1[name])) {
         if (!isObject(object2[name])) {
-          resultArrayOfStrings.push(
+          return (
             `Property '${cumulativePath.slice(
               1,
               cumulativePath.length,
             )}' was updated. From ${ifStringAddApostrophe(
               object1[name],
-            )} to ${ifStringAddApostrophe(object2[name])}`,
-          );
-        } else {
-          resultArrayOfStrings.push(
-            `Property '${cumulativePath.slice(
-              1,
-              cumulativePath.length,
-            )}' was updated. From ${ifStringAddApostrophe(
-              object1[name],
-            )} to [complex value]`,
-          );
+            )} to ${ifStringAddApostrophe(object2[name])}`);
         }
-      } else if (!isObject(object2[name])) {
-        resultArrayOfStrings.push(
+        return (
+          `Property '${cumulativePath.slice(
+            1,
+            cumulativePath.length,
+          )}' was updated. From ${ifStringAddApostrophe(
+            object1[name],
+          )} to [complex value]`);
+      }
+      if (!isObject(object2[name])) {
+        return (
           `Property '${cumulativePath.slice(
             1,
             cumulativePath.length,
           )}' was updated. From [complex value] to ${ifStringAddApostrophe(
             object2[name],
-          )}`,
-        );
-      } else {
-        resultArrayOfStrings.push(
-          plain(object1[name], object2[name], cumulativePath),
-        );
+          )}`);
       }
+      return (
+        plain(object1[name], object2[name], cumulativePath));
     }
+    return [];
   });
 
-  return resultArrayOfStrings;
+  return _.remove(resultArrayOfStrings);
 };
 
 export default plain;
