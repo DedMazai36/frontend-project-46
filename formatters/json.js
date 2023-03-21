18c7ad3bcefc2ce3b15af188572d5e713e588d60
Andrey Koliukh <kolldronzr@yandex.ru>
2023-03-21 12:41:39 +0600

    fix json.js

--- formatters/json.js
+++ formatters/json.js
@@ -1,45 +1,57 @@
+/* eslint-disable no-use-before-define */
 import _ from 'lodash';
 import { getFiltredNames, isObject } from './formattersEngine.js';
 
+const getStatus = (object2, arrayOfNames, name) => {
+  const [filtredNamesOfObject1,,
+    filtredNamesOfObject2] = arrayOfNames;
+
+  if (filtredNamesOfObject1.includes(name)) {
+    if (!Object.getOwnPropertyNames(object2).includes(name)) {
+      return 'removed';
+    }
+    return 'notChanged';
+  }
+  if (filtredNamesOfObject2.includes(name)) {
+    return 'added';
+  }
+
+  return 'updated';
+};
+
+const getValue = (object1, object2, arrayOfNames, name) => {
+  const [filtredNamesOfObject1,
+    filtredNamesOfIntersection,
+    filtredNamesOfObject2] = arrayOfNames;
+
+  if (filtredNamesOfObject1.includes(name)) {
+    if (Object.getOwnPropertyNames(object2).includes(name)) {
+      return _.cloneDeep(object1[name]);
+    }
+  }
+  if (filtredNamesOfObject2.includes(name)) {
+    return _.cloneDeep(object2[name]);
+  }
+  if (filtredNamesOfIntersection.includes(name)) {
+    if (isObject(object1[name]) && isObject(object2[name])) {
+      return json(
+        _.cloneDeep(object1[name]),
+        _.cloneDeep(object2[name]),
+      );
+    }
+  }
+  return _.cloneDeep(object2[name]);
+};
+
 const json = (object1, object2) => {
   const arrayOfNames = getFiltredNames(object1, object2);
-  const filtredNamesOfObject1 = arrayOfNames[0];
-  const filtredNamesOfIntersection = arrayOfNames[1];
-  const filtredNamesOfObject2 = arrayOfNames[2];
-  const arrayForSort = [...arrayOfNames].flat(10).sort();
-  // const resultArray = [];
+  const arrayForSort = _.sortBy(arrayOfNames.flat(10));
 
-  const resultArray = arrayForSort.map((name) => {
-    const resultObject = {};
-    resultObject.key = name;
-
-    if (filtredNamesOfObject1.includes(name)) {
-      if (!Object.getOwnPropertyNames(object2).includes(name)) {
-        resultObject.status = 'removed';
-      } else {
-        resultObject.status = 'notChanged';
-        resultObject.value = _.cloneDeep(object1[name]);
-      }
-    }
-    if (filtredNamesOfObject2.includes(name)) {
-      resultObject.status = 'added';
-      resultObject.value = _.cloneDeep(object2[name]);
-    }
-    if (filtredNamesOfIntersection.includes(name)) {
-      if (isObject(object1[name]) && isObject(object2[name])) {
-        resultObject.status = 'updated';
-        resultObject.value = json(
-          _.cloneDeep(object1[name]),
-          _.cloneDeep(object2[name]),
-        );
-      } else {
-        resultObject.status = 'updated';
-        resultObject.value = _.cloneDeep(object2[name]);
-      }
-    }
-
-    return _.cloneDeep(resultObject);
-  });
+  const resultArray = arrayForSort.map((name) => ({
+    key: name,
+    status: getStatus(object2, arrayOfNames, name),
+    value: getValue(object1, object2, arrayOfNames, name),
+  }));
 
   return resultArray;
 };
